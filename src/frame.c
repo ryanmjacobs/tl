0aa4e79b3b3b24f887fc2b58f0be52fe0fef8eda
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-03-21 21:17:21 -0700

    support differnt display depths

--- src/frame.c
+++ src/frame.c
@@ -14,10 +14,22 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
+#include <stdint.h>
+#include <sys/param.h>
+
 #include "frame.h"
+
+struct pixel_t {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+};
+
+typedef uint16_t WORD;
 
 static Screen  *scr;
 static Display *dpl;
+static struct pixel_t grab_pixel(XImage *img, int x, int y);
 
 int init_x(const char *display_name) {
     dpl = XOpenDisplay(display_name);
@@ -31,10 +43,19 @@ int init_x(const char *display_name) {
 
     return 0;
 }
+
 int free_x(void) {
     XCloseDisplay(dpl);
 
     return 0;
+}
+
+int get_frame_width(void) {
+    return XWidthOfScreen(scr);
+}
+
+int get_frame_height(void) {
+    return XHeightOfScreen(scr);
 }
 
 unsigned char *grab_frame(void) {
@@ -51,14 +72,11 @@ unsigned char *grab_frame(void) {
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            unsigned long pixel = XGetPixel(img, x, y);
-            unsigned char blue  = (pixel & img->blue_mask)  >> 0;
-            unsigned char green = (pixel & img->green_mask) >> 8;
-            unsigned char red   = (pixel & img->red_mask)   >> 16;
+            struct pixel_t p = grab_pixel(img, x, y);
 
-            rgb[3*(x + width*y) + 0] = red;
-            rgb[3*(x + width*y) + 1] = green;
-            rgb[3*(x + width*y) + 2] = blue;
+            rgb[3*(x + width*y) + 0] = p.red;
+            rgb[3*(x + width*y) + 1] = p.green;
+            rgb[3*(x + width*y) + 2] = p.blue;
         }
     }
 
@@ -66,10 +84,39 @@ unsigned char *grab_frame(void) {
     return rgb;
 }
 
-int get_frame_width(void) {
-    return XWidthOfScreen(scr);
-}
+// http://www.opensource.apple.com/source/tcl/tcl-95/tk/tk/win/tkWinImage.c
+static struct pixel_t grab_pixel(XImage *img, int x, int y) {
+    struct pixel_t p = {0};
+    unsigned char *srcPtr = &(img->data[(y * img->bytes_per_line)
+        + ((x * img->bits_per_pixel) / NBBY)]);
 
-int get_frame_height(void) {
-    return XHeightOfScreen(scr);
+    switch (img->bits_per_pixel) {
+        case 32:
+        case 24:
+            p.red   = srcPtr[2];
+            p.green = srcPtr[1];
+            p.blue  = srcPtr[0];
+            break;
+        case 16:
+            p.red   = ((((WORD*)srcPtr)[0]) >> 7) & 0xf8;
+            p.green = ((((WORD*)srcPtr)[0]) >> 2) & 0xf8;
+            p.blue  = ((((WORD*)srcPtr)[0]) << 3) & 0xf8;
+            break;
+      //case 8:
+      //    pixel = srcPtr[0];
+      //    break;
+      //case 4:
+      //    pixel = ((x%2) ? (*srcPtr) : ((*srcPtr) >> 4)) & 0x0f;
+      //    break;
+      //case 1:
+      //    pixel = ((*srcPtr) & (0x80 >> (x%8))) ? 1 : 0;
+      //    break;
+        default:
+            fprintf(stderr, "error: %d-bit pixel displays are not yet "
+                    "supported. Go file a bug.\n", img->bits_per_pixel);
+            exit(1);
+            break;
+    }
+
+    return p;
 }
